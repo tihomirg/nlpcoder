a3277aa0a9a51cc2e1d7516db55ccbafac05d451
Tihomir Gvero <tihomir.gvero@gmail.com>
2014-01-22 13:45:34 +0100

    Signed-off-by: Tihomir Gvero <tihomir.gvero@gmail.com>

---
+++ LearningPCFG/test/CityImpl.java
@@ -0,0 +1,95 @@
+package com.ajah.geo;
+
+import java.io.*;
+
+/**
+ * Skeleton implementation of City interface. Usable as-is or suitable for
+ * extension.
+ * 
+ * @author Eric F. Savage <code@efsavage.com>
+ */
+public class CityImpl implements City {
+
+	protected String id;  //My File
+
+	protected String name;
+
+	protected State state;
+
+	protected Country country = null;
+
+	/**
+	 * If state is set, will return state.getCountry(), otherwise will return
+	 * local country field.
+	 * 
+	 * @see State#getCountry()
+	 * @return Country object as set via setCountry or from state, may be null.
+	 */
+	@Override
+	public Country getCountry() {
+		if (this.state == null) {
+			return this.country;
+		}
+		return this.state.getCountry();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String getId() {
+		return this.id;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String getName() {
+		return this.name;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public State getState() {
+		return this.state;
+	}
+
+	/**
+	 * Sets the ID.
+	 * 
+	 * @param id
+	 */
+	public void setId(final String id) {
+		this.id = id; //Righthand-side comment under
+	}
+
+	/**
+	 * Sets the name.
+	 * 
+	 * @param name
+	 */
+	public void setName(final String name) {
+		//Comment under
+		this.name = name;
+		//System.out.print("A"+this.name+"B"+"\n");
+		
+		for(int i= 0; i <10;i++){
+			System.out.print("A");
+		}
+	}
+
+	/**
+	 * Sets the state.
+	 * 
+	 * @param state
+	 */
+	public void setState(final State state) {
+		this.state = state;
+		
+		// Comment below
+	}
+
+}
