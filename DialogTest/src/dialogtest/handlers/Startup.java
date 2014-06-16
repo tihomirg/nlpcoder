f09900ac7fc6c1f8d9c6b84c8cc6101fe0655dca
gvero <tihomir.gvero@gmail.com>
2014-06-16 17:25:48 +0200

    Signed-off-by: gvero <tihomir.gvero@gmail.com>

---
+++ DialogTest/src/dialogtest/handlers/Startup.java
@@ -0,0 +1,13 @@
+package dialogtest.handlers;
+
+import org.eclipse.ui.IStartup;
+
+public class Startup implements IStartup {
+
+	@Override
+	public void earlyStartup() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
