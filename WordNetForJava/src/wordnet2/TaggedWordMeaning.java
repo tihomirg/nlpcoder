8db0403e1e7c8826e896791227547de17eef218f
gvero <tihomir.gvero@gmail.com>
2014-07-09 19:18:22 +0200

    Signed-off-by: gvero <tihomir.gvero@gmail.com>

---
+++ WordNetForJava/src/wordnet2/TaggedWordMeaning.java
@@ -0,0 +1,43 @@
+package wordnet2;
+
+import java.util.List;
+import nlp.parser.TaggedWord;
+
+public class TaggedWordMeaning {
+
+	private String gloss;
+	private double score;
+	private List<TaggedWord> words;
+	
+	public TaggedWordMeaning(String gloss, double score, List<TaggedWord> words) {
+		this.gloss = gloss;
+		this.score = score;
+		this.words = words;
+	}
+
+	public String getGloss() {
+		return gloss;
+	}
+
+	public void setGloss(String gloss) {
+		this.gloss = gloss;
+	}
+
+	public double getScore() {
+		return score;
+	}
+
+	public void setScore(double score) {
+		this.score = score;
+	}
+
+	public List<TaggedWord> getWords() {
+		return this.words;
+	}
+	
+	@Override
+	public String toString() {
+		return "WordMeaning [score=" + score + ", gloss=" + gloss + ", \nwords="+ words + "]";
+	}
+
+}
