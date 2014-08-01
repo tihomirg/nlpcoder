7457f9936ec52294ab12854afefdd1671e0cb367
gvero <tihomir.gvero@gmail.com>
2014-08-01 21:35:37 +0200

    Signed-off-by: gvero <tihomir.gvero@gmail.com>

---
+++ SearchEngine/src/tests/ParserForDisjointSubgroupsTest.java
@@ -0,0 +1,162 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import nlp.parser.Token;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import search.WToken;
+import search.nlp.parser.DisjointSubgroups;
+import search.nlp.parser.ParserForDisjointSubgroups;
+import search.nlp.parser.Subgroup;
+
+public class ParserForDisjointSubgroupsTest {
+
+	private static ParserForDisjointSubgroups parserForIndexes;
+
+	@BeforeClass
+	public static void beforeClass() {
+		parserForIndexes = new ParserForDisjointSubgroups();
+	}
+	
+	@Before
+	public void beforeTest() {
+		System.out.println();
+		System.out.println("Test --------------------------------------------------------------------------------");
+		System.out.println();
+	}	
+
+	private static Subgroup createSubgroup(Token[] tokens) {
+		List<WToken> wTokens = new LinkedList<WToken>();
+		for (Token token: tokens) {
+			wTokens.add(new WToken(token));
+		}
+		return new Subgroup(wTokens);
+	}
+
+	private static Token createNounT(String lemma) {
+		return new Token(lemma, lemma, "N");
+	}
+
+	private Token createVerbT(String lemma) {
+		return new Token(lemma, lemma, "V");
+	}	
+
+	private List<Subgroup> createSubgorups(Token[][] tokenss) {
+		List<Subgroup> subgroups = new LinkedList<Subgroup>();	
+		for (Token[] tokens : tokenss) {
+			subgroups.add(createSubgroup(tokens));
+		}
+		return subgroups;
+	}
+
+	@Test
+	public void test1() {
+		Token[][] tokenss = new Token[][]{
+				{createNounT("file")}, 
+				{createNounT("line")}, 
+				{createNounT("stream")}, 
+				{createNounT("file")}, 
+				{createNounT("socket")}};
+
+		List<Subgroup> subgroups = createSubgorups(tokenss);
+		List<DisjointSubgroups> disjointSubgroupss = parserForIndexes.calculateDisjointSubgroups(subgroups);
+		
+		for (DisjointSubgroups disjointSubgroups : disjointSubgroupss) {
+			System.out.println(disjointSubgroups);
+		}
+	}
+	
+	@Test
+	public void test2() {
+		Token[][] tokenss = new Token[][]{
+				{createNounT("file"), createNounT("line")}, 
+				{createNounT("line")}, 
+				{createNounT("stream")}, 
+				{createNounT("file")}, 
+				{createNounT("socket")}};
+
+		List<Subgroup> subgroups = createSubgorups(tokenss);
+		List<DisjointSubgroups> disjointSubgroupss = parserForIndexes.calculateDisjointSubgroups(subgroups);
+		
+		for (DisjointSubgroups disjointSubgroups : disjointSubgroupss) {
+			System.out.println(disjointSubgroups);
+		}
+	}
+	
+	@Test
+	public void test3() {
+		Token[][] tokenss = new Token[][]{
+				{createVerbT("file")}, 
+				{createNounT("line")}, 
+				{createNounT("stream")}, 
+				{createNounT("file")}, 
+				{createNounT("socket")}};
+
+		List<Subgroup> subgroups = createSubgorups(tokenss);
+		List<DisjointSubgroups> disjointSubgroupss = parserForIndexes.calculateDisjointSubgroups(subgroups);
+		
+		for (DisjointSubgroups disjointSubgroups : disjointSubgroupss) {
+			System.out.println(disjointSubgroups);
+		}
+	}
+	
+	@Test
+	public void test4() {
+		Token[][] tokenss = new Token[][]{
+				{createNounT("file"), createNounT("system")}, 
+				{createNounT("line"), createNounT("class")}, 
+				{createNounT("stream")}, 
+				{createNounT("file"), createNounT("line")}, 
+				{createNounT("socket")}};
+
+		List<Subgroup> subgroups = createSubgorups(tokenss);
+		List<DisjointSubgroups> disjointSubgroupss = parserForIndexes.calculateDisjointSubgroups(subgroups);
+		
+		for (DisjointSubgroups disjointSubgroups : disjointSubgroupss) {
+			System.out.println(disjointSubgroups);
+		}
+	}
+	
+	@Test
+	public void test5() {
+		Token[][] tokenss = new Token[][]{
+				{createNounT("file"), createNounT("system")}, 
+				{createNounT("line"), createNounT("class")}, 
+				{createNounT("stream")}, 
+				{createNounT("file"), createNounT("line")}, 
+				{createNounT("socket")},
+				{createVerbT("file")}};
+
+		List<Subgroup> subgroups = createSubgorups(tokenss);
+		List<DisjointSubgroups> disjointSubgroupss = parserForIndexes.calculateDisjointSubgroups(subgroups);
+		
+		for (DisjointSubgroups disjointSubgroups : disjointSubgroupss) {
+			System.out.println(disjointSubgroups);
+		}
+	}
+	
+	@Test
+	public void test6() {
+		Token[][] tokenss = new Token[][]{
+				{createNounT("file"), createNounT("system")}, 
+				{createNounT("line"), createNounT("class")}, 
+				{createNounT("stream")}, 
+				{createNounT("file"), createNounT("line")}, 
+				{createNounT("socket")},
+				{createVerbT("file"), createNounT("socket")}};
+
+		List<Subgroup> subgroups = createSubgorups(tokenss);
+		List<DisjointSubgroups> disjointSubgroupss = parserForIndexes.calculateDisjointSubgroups(subgroups);
+		
+		for (DisjointSubgroups disjointSubgroups : disjointSubgroupss) {
+			System.out.println(disjointSubgroups);
+		}
+	}	
+}
